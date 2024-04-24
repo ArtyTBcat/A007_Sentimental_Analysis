47ed107558c03e6a997cb4e17697e2257139ff50
Arty <bunluesriruangnontakorn@gmail.com>
2024-04-24 18:19:49 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import json, os
+import pandas as pd
+
+file_path = os.path.join("E:/Necleotide Codes/archive", "yelp_academic_dataset_checkin.json")
+data_file = open(file_path)
+data = []
+for line in data_file:
+    data.append(json.loads(line))
+checkin_df = pd.DataFrame(data)
+print(checkin_df)
+data_file.close()
